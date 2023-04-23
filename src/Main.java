c350370776c75b5b5bc5c23b0899a90481482db2
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-23 19:08:24 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {7, 1, 5, 3, 6, 4};
+        Solution solution = new Solution();
+        System.out.println(solution.maxProfit(arr));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Solution {
+public class Solution
+{
+    public int maxProfit(int[] prices)
+    {
+        int minPrice = prices[0], maxProfit = 0, profit;
+        for(int i = 1; i < prices.length; i++)
+        {
+            minPrice = Math.min(minPrice, prices[i]);
+            profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
 }
